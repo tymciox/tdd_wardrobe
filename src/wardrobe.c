00496c1e5f7cd0824268c76acfa14bb93a6d170a
Tymoteusz Hurko <tymoteusz.hurko@andra.com.pl>
2022-12-18 21:19:39 +0100

    green: negative not allowed

--- src/wardrobe.c
+++ src/wardrobe.c
@@ -2,6 +2,7 @@
 #include "wardrobe.h"
 #include <string.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 void init(void)
 {
@@ -13,6 +14,7 @@ int add(char *numbers)
     int sum = 0;
     char *num_ptr = NULL;
     char delimiters[5] = {0};
+    bool negative_number = false;
 
     if (NULL == numbers || '\0' == *numbers)
     {
@@ -35,9 +37,15 @@ int add(char *numbers)
     num_ptr--;
     do
     {
+        if (atoi(num_ptr+1)<0) 
+        {
+            printf("negatives not allowed: %d\r\n", atoi(num_ptr+1));
+            negative_number = true;
+        }
         sum += atoi(num_ptr+1);
         num_ptr = strpbrk(num_ptr+1, delimiters);
     } while (num_ptr != NULL);
     
+    if (true == negative_number) return 0;
     return sum;
 }
