e3a49f32a4133b9aad3988eb9ee9aec7f23f652c
Tymoteusz Hurko <tymoteusz.hurko@andra.com.pl>
2022-12-18 21:50:50 +0100

    refactor: negative number

--- src/wardrobe.c
+++ src/wardrobe.c
@@ -6,7 +6,11 @@
 
 #define IS_EMPTY(a) (NULL == a || '\0' == *a)
 #define DEFAULT_DELIMITER   ','
+#define START_FROM_BEGIN(a)  (a-1)   
+#define SKIP_FIRST_LINE(a)  (a+3)
+
 static int set_delimiters(const char *const numbers, char *delimiters);
+static bool check_if_parameter_is_negative(char *num_ptr);
 
 void init(void)
 {
@@ -20,37 +24,40 @@ int add(char *numbers)
     char delimiters[5] = {0};
     bool negative_number = false;
 
-    if (IS_EMPTY(numbers))
-    {
-        return 0;
-    }
+    if (IS_EMPTY(numbers)) return 0;
 
-    if (set_delimiters(numbers, delimiters) == 1) return 0;
+    if (set_delimiters(numbers, delimiters) != 0) return 0;
 
     if (delimiters[0] != DEFAULT_DELIMITER)
     {
-        num_ptr = numbers + 3;
+        num_ptr = SKIP_FIRST_LINE(numbers);
     }
     else
     {
-        num_ptr = numbers - 1;
+        num_ptr = START_FROM_BEGIN(numbers);
     }
 
     do
     {
-        if (atoi(num_ptr+1)<0) 
-        {
-            printf("negatives not allowed: %d\r\n", atoi(num_ptr+1));
-            negative_number = true;
-        }
-        sum += atoi(num_ptr+1);
-        num_ptr = strpbrk(num_ptr+1, delimiters);
+        num_ptr++;
+        if (check_if_parameter_is_negative(num_ptr)) negative_number = true;
+        sum += atoi(num_ptr);
+        num_ptr = strpbrk(num_ptr, delimiters);
     } while (num_ptr != NULL);
     
     if (true == negative_number) return 0;
     return sum;
 }
 
+static bool check_if_parameter_is_negative(char *num_ptr)
+{
+    if (atoi(num_ptr)<0) 
+    {
+        printf("negatives not allowed: %d\r\n", atoi(num_ptr));
+        return 1;
+    }
+    return 0;
+}
 
 static int set_delimiters(const char *const numbers, char *delimiters)
 {
