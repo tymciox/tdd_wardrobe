d44f0c33cfe2c71999dfdc49414568385c25fbc8
Tymoteusz Hurko <tymoteusz.hurko@andra.com.pl>
2022-12-18 19:27:43 +0100

    green: adding two number

--- src/wardrobe.c
+++ src/wardrobe.c
@@ -1,17 +1,29 @@
 #include <stdio.h>
 #include "wardrobe.h"
+#include <string.h>
+#include <stdlib.h>
 
 void init(void)
 {
 
 }
-
 int add(char *numbers)
 {
+    int sum = 0;
+    char *num_ptr = NULL;
+
     if (NULL == numbers || '\0' == *numbers)
     {
         return 0;
     }
+
+    num_ptr = numbers;
+    sum = atoi(num_ptr);
+    do
+    {
+        sum += atoi(num_ptr+1);
+        num_ptr = strchr(num_ptr+1, ',');
+    } while (num_ptr != NULL);
     
-    return 1;
+    return sum;
 }
